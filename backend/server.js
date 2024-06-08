5383a6bfd3b4dd34cb355ce6c3b0ee726e79304a
Abhishek mishra <abhishekmishra992016@gmail.com>
2024-06-08 16:40:46 +0530

    mongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const cors = require("cors");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 const app = express();
 dotenv.config();
+connectDB();
 app.use(cors());
 app.use(express.json());
 
@@ -20,5 +23,5 @@ app.get("/api/chat", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`.yellow.bold);
 });
